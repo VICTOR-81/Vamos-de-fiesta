cd7e1c075e3ca9bd376b29e824b858b41791f717
VICTOR-81 <v161181@gmail.com>
2021-07-10 01:30:45 +0700

    upgrade

--- mail.php
+++ mail.php
@@ -7,7 +7,7 @@ $message = $_POST['message'];
 $mes = "Имя: $name \nE-mail: $email \nТекст: $message";
 // Пытаемся отправить письмо по заданному адресу
 // Если нужно, чтобы письма всё время уходили на ваш адрес — замените первую переменную $email на свой адрес электронной почты
-$send = mail("v161181gmail.com", "", $mes, "Content-type:text/plain; charset = UTF-8\r\nFrom:$email");
+$send = mail("blackbeepd@gmail.com", "", $mes, "Content-type:text/plain; charset = UTF-8\r\nFrom:$email");
 // Если отправка прошла успешно — так и пишем
 if ($send == 'true') {echo "Сообщение отправлено"; echo "<meta http-equiv='refresh' content='5; url=http://blackbee.biz/'>";}
 // Если письмо не ушло — выводим сообщение об ошибке
